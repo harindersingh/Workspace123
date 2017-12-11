d7fcbad3dd83e434fe314cd73a654eb567a64d1e
harindersingh <im.harinder.singh@gmail.com>
2017-12-11 23:39:49 +0530

    Auth setup using JWT | virtual environment setup

---
+++ RESTinFlask/code/app.py
@@ -0,0 +1,51 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required, current_identity
+
+from security import authenticate, identity
+
+app = Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS'] = TRUE
+app.secret_key = 'panda'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
+
+items = []
+
+#no need to do jsonify when using flask-restful
+
+class Item(Resource):
+	parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+
+    @jwt_required()
+	def get(self, name):
+		'''for item in items:
+			if item['name'] == name:
+				return item
+		return {'item': None}, 404'''
+		item = next(filter(lambda x: x['name'] == name, items), None)
+		return {'item': item}, 200 if item else 404
+
+	def post(self, name):
+		#check duplication of items
+		if next(filter(lambda x: x['name'] == name, items), None) is not None:
+			return {'message': "An item with name '{}' already exists".format(name)}, 400
+		data = request.get_json()
+		item = {'name': name, 'price': data['price']}
+		items.append(item)
+		return item, 201
+
+class ItemList(Resource):
+	def get(self):
+		return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')		# http://127.0.0.1:5000/item/<name>
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
---
+++ RESTinFlask/code/user.py
@@ -0,0 +1,5 @@
+class User(object):
+	def __init(self, id, username, password):
+		self.id = id
+		self.username = username
+		self.password = password
